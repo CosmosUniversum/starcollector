fdb8838007c38202f8bef6483207f4a19fedd55c
Josh Castillo <John.Joshua.Castillo@gmail.com>
2022-01-12 18:28:02 -0500

    render star objects in database in index view

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
+from .models import Star
 
-class Star:
-  def __init__(self, name, constellation, type, distance):
-      self.name = name
-      self.constellation = constellation
-      self.type = type
-      self.distance = distance
-
-stars = [
-  Star('Vega', 'Lyra', 'main-sequence', '25'),
-  Star('Betelgeuse', 'Orion', 'Red Supergiant', '500-600'),
-  Star('Antares', 'Scorpius', 'Red Supergiant', '550')
-]
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 def about(request):
   return render(request, 'about.html')
 def stars_index(request):
+  stars = Star.objects.all()
   return render(request, 'stars/index.html', {'stars': stars})
