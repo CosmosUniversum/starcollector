6c6fabec6ca8b2da4188578c691b56d836ba8435
Josh Castillo <John.Joshua.Castillo@gmail.com>
2022-01-12 18:57:41 -0500

    add star details page view

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('stars/', views.stars_index, name='stars_index')
+  path('stars/', views.stars_index, name='stars_index'),
+  path('stars/<int:star_id>/', views.stars_detail, name='stars_detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,8 +4,14 @@ from .models import Star
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
 def about(request):
   return render(request, 'about.html')
+
 def stars_index(request):
   stars = Star.objects.all()
   return render(request, 'stars/index.html', {'stars': stars})
+
+def stars_detail(request, star_id):
+  star = Star.objects.get(id=star_id)
+  return render(request, 'stars/detail.html', {'star': star})
