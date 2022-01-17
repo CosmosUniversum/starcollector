85ffd500f867058ac42adef119848648227448f3
Josh Castillo <John.Joshua.Castillo@gmail.com>
2022-01-16 23:19:37 -0500

    add create exoplanet functionality to Star Detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from dataclasses import field
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Star
 from main_app import models
@@ -20,6 +20,14 @@ def stars_detail(request, star_id):
   star = Star.objects.get(id=star_id)
   exoplanet_form = ExoplanetForm()
   return render(request, 'stars/detail.html', {'star': star, 'exoplanet_form': exoplanet_form})
+
+def add_exoplanet(request, star_id):
+  form = ExoplanetForm(request.POST)
+  if form.is_valid():
+    new_exoplanet = form.save(commit=False)
+    new_exoplanet.star_id = star_id
+    new_exoplanet.save()
+    return redirect('stars_detail', star_id=star_id)
 
 class StarCreate(CreateView):
   model = Star
